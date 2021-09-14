c6c8671a1de2dd0f79b7121f291cecb25437261e
RajendraGund <rajendragund2015@gmail.com>
2021-09-14 23:11:18 +0530

    Added : Welcome message for the User

---
+++ src/main/java/com/bridgelabz/regex/RegexURP.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.regex;
+
+public class RegexURP {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the User Registration Problem using Regex!!");
+    }
+}
