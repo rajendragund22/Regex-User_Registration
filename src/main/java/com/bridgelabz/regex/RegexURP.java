5acd0367030acf90723e9d3f57b3d721808f3fe6
RajendraGund <rajendragund2015@gmail.com>
2021-09-14 23:36:40 +0530

    Added : Program for the Check whether first name is valid or not

--- src/main/java/com/bridgelabz/regex/RegexURP.java
+++ src/main/java/com/bridgelabz/regex/RegexURP.java
@@ -3,5 +3,8 @@ package com.bridgelabz.regex;
 public class RegexURP {
     public static void main(String[] args) {
         System.out.println("Welcome to the User Registration Problem using Regex!!");
+        UserOperator user = new UserOperator();
+        user.firstName("rajendra");
+
     }
 }
---
+++ src/main/java/com/bridgelabz/regex/UserOperator.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserOperator {
+    public void firstName(String firstName){
+        String regex = "^[A-Z]{1}[a-z]{2,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(firstName);
+        boolean valid=matcher.matches();
+        if (valid==true){
+            System.out.println("Your name "+firstName+" is Valid");
+        }
+        else{
+            System.out.println("Your name "+firstName+" is Invalid");
+        }
+    }
+}
