bd01a15c52d1476e6ceec181c86ed727018414b7
RajendraGund <rajendragund2015@gmail.com>
2021-09-15 00:20:46 +0530

    Added : Program (Rule 1) for the Passward where passward should have minimum 8 char

--- src/main/java/com/bridgelabz/regex/RegexURP.java
+++ src/main/java/com/bridgelabz/regex/RegexURP.java
@@ -8,5 +8,6 @@ public class RegexURP {
         user.lastName("Gund");
         user.emailaddress("rajendra@gmail.com");
         user.phoneNo("91 9604315270");
+        user.passRule1("123asASD");
     }
 }
